bb48fbb8ef2970b20cabe3f9f946a7d9f8a6ff69
guoqiang <1099594257@qq.com>
2018-09-25 00:06:40 +0800

    footer

---
+++ src/pages/h5/footer/footer.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="h5-footer-container">
+    <div class="footer-row">
+      <div class="row-pick-up">
+        <div class="row__title">服务</div>
+        <div class="row__pick__arrow">下拉</div>
+      </div>
+      <div class="row-content">
+        <router-link :to="{name: 'trading'}">{{ $t('trading') }}</router-link>
+        <a
+          :href="aboutLink"
+          target="_blank">{{ $t('footer_about') }}</a>
+        <!-- <a href="#" target="_blank">{{$t('footer_help')}}</a> -->
+        <a
+          :href="requestLink"
+          target="_blank">{{ $t('footer_request') }}</a>
+      </div>
+    </div>
+    <div class="footer-row">
+      <div class="row-pick-up">
+        <div class="row__title">工具</div>
+        <div class="row__pick__arrow">下拉</div>
+      </div>
+      <div class="row-content">
+        <a
+          :href="announcementLink"
+          target="_blank">{{ $t('footer_notice') }}</a>
+        <a
+          :href="helpLink"
+          target="_blank">{{ $t('user_guide') }}</a>
+
+      </div>
+    </div>
+    <div class="footer-row">
+      <div class="row-pick-up">
+        <div class="row__title">说明</div>
+        <div class="row__pick__arrow">下拉</div>
+      </div>
+      <div class="row-content">
+        <router-link
+          :to="{name: 'terms'}"
+          target="_blank">{{ $t('footer_agreement') }}</router-link>
+        <router-link
+          :to="{name: 'PrivacyPolicy'}"
+          target="_blank">{{ $t('footer_private') }}</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import './footer.scss'
+export default {
+
+}
+</script>
--- src/pages/h5/index/index.vue
+++ src/pages/h5/index/index.vue
@@ -29,12 +29,64 @@
       </div>
       <pair-table/>
     </div>
+    <h5-footer/>
+    <div class="h5-contact-info">
+      <div class="contact-list">
+        <a
+          class="contact-item-wrapper mr-20 pointer"
+          href='https://www.facebook.com/IXExchange'>
+          <icon
+            class="contact-item"
+            name="footer-facebook"/>
+        </a>
+        <a
+          class="contact-item-wrapper mr-20 pointer"
+          href='https://twitter.com/IXExchange'>
+          <icon
+            class="contact-item"
+            name="footer-twitter"/>
+        </a>
+
+        <a
+          href="mailto:service@ix.com"
+          class="contact-item-wrapper mr-20 pointer">
+          <icon
+            class="contact-item"
+            name="footer-email"/>
+        </a>
+        <div class="contact-item-wrapper mr-20">
+          <icon
+            class="contact-item"
+            name="footer-wx"/>
+          <img
+            class="contact-img"
+            src="../../../assets/pic-contact-wx.jpg">
+        </div>
+        <a
+          class="contact-item-wrapper mr-20 pointer"
+          href='https://t.me/ixofficial'>
+          <icon
+            class="contact-item "
+            name="footer-telegram"/>
+        </a>
+        <div class="contact-item-wrapper">
+          <icon
+            class="contact-item"
+            name="footer-biyong"/>
+          <img
+            class="contact-img"
+            src="../../../assets/pic-contact-biyong.jpg">
+        </div>
+      </div>
+      <p class="copyright">Copyright © 2018 IX.COM</p>
+    </div>
   </div>
 </template>
 <script>
 import './index.scss'
 import MineSummary from '../mine-summary'
 import PairTable from '@/components/Trading/PairTable'
+import h5Footer from '../footer'
 
 export default {
   data () {
@@ -44,7 +96,8 @@ export default {
   },
   components: {
     MineSummary,
-    PairTable
+    PairTable,
+    h5Footer
   }
 }
 </script>
