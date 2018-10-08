76f789fb729c7fa6c8af1058915470558d3beb47
aaaa <15117983688@163.com>
2018-10-08 15:19:27 +0800

    费率页面翻译

--- src/pages/ProfileRate.vue
+++ src/pages/ProfileRate.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="user-center-right">
     <div class="profile-container">
-      <div class="rate-tit">交易手续费</div>
+      <div class="rate-tit">{{ $t('transaction_fees') }}</div>
       <div class="rate-tab">
         <div class="rate-tab-tit">
-          <p class="lt rate-t-a">交易对</p>
-          <p class="lt rate-t-b">挂单</p>
-          <p class="lt rate-t-c">吃单</p>
+          <p class="lt rate-t-a">{{ $t('transaction_pair') }}</p>
+          <p class="lt rate-t-b">{{ $t('maker') }}</p>
+          <p class="lt rate-t-c">{{ $t('taker') }}</p>
         </div>
         <ul class="rate-ul">
           <li>
@@ -27,12 +27,12 @@
         </ul>
       </div>
       <div class="rate-tips">
-        <p class="rate-tips-title">提示</p>
-        <p>1、挂单是你所下的限价单并未与当前挂单成交，并被放在买卖盘中等待成交的摆单，它增加了买卖盘的流动性。</p>
-        <p>2、当其他人的摆单主动与你所下的摆单成交，你将支付挂单交易手续费（请注意当其他人的限价单与你的限价单成交，而下单时间又早于你时，你将支付吃单费）。</p>
-        <p>3、吃单是你所下的限价单或市价单与当前的挂单直接成交。</p>
-        <p>4、当你所下的摆单主动与其他人的摆单成交，你将支付吃单交易手续费。</p>
-        <p>5、交易手续费将从您的成交总额中扣除。若成交后获得比特币资产，则支付比特币交易手续费。</p>
+        <p class="rate-tips-title">{{ $t('tips') }}</p>
+        <p>{{ $t('rate_tips_a') }}</p>
+        <p>{{ $t('rate_tips_b') }}</p>
+        <p>{{ $t('rate_tips_c') }}</p>
+        <p>{{ $t('rate_tips_d') }}</p>
+        <p>{{ $t('rate_tips_e') }}</p>
       </div>
     </div>
   </div>
