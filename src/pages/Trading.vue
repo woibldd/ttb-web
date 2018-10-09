bd5540ad8c30adec3cabd79f5410dacdbbe61adf
aaaa <15117983688@163.com>
2018-10-09 14:15:55 +0800

    交易页面调整

--- src/pages/Trading.vue
+++ src/pages/Trading.vue
@@ -7,7 +7,7 @@
       v-if="!isMobile"/>
     <mobile-nav
       v-if="isMobile"
-      />
+    />
     <div class="container-trade-panel">
       <div class="ix-row">
         <div
@@ -28,9 +28,15 @@
           <div
             class="ix-grid ix-grid-tv"
             ref="gridTradingView">
-            <!-- <div class="grid-title">
-              {{state.pro.pairTick}}
-            </div> -->
+            <div class="grid-title">
+              <p class="grid-currency">BTC/USDT</p>
+              <p class="grid-value">6332.47</p>
+              <p class="grid-abt">≈43775.68</p>
+              <p class="grid-increase">{{ $t('increase') }}<i>-0.48%</i></p>
+              <p class="grid-high">{{ $t('secure_level_2') }} 6949.28</p>
+              <p class="grid-low">{{ $t('secure_level_0') }} 6949.28</p>
+              <p class="grid-tf-amount">24H{{ $t('vol') }} 184225 BTC</p>
+            </div>
             <TradingView ref="TradingView"/>
             <div
               class="active-box"
@@ -419,6 +425,42 @@ export default {
 }
 .grid-title {
   height: 56px;
+  line-height: 56px;
+  box-sizing: border-box;
+  padding-left: 20px;
+  p{
+    float: left;
+  }
+  .grid-currency{
+    color: #fff;
+    font-size: 20px;
+    margin-right: 20px;
+    font-weight: bold;
+  }
+  .grid-value{
+    font-size: 20px;
+    color: #09C989;
+    font-weight: bold;
+    margin-right: 10px;
+  }
+  .grid-abt{
+    font-size: 14px;
+    color: #09C989;
+    margin-right: 20px;
+  }
+  .grid-increase{
+    font-size: 14px;
+    color: #fff;
+    margin-right: 20px;
+    i{
+      color: #F24E4D;
+    }
+  }
+  .grid-high,.grid-low,.grid-tf-amount{
+    font-size: 14px;
+    color: #D7D7D7;
+    margin-right: 20px;
+  }
 }
 .active-box {
   position: absolute;
