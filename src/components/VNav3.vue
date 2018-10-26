8581131438bb7245b9e16cdfa01f28d16c88ed76
mylhei <mylhei@gmail.com>
2018-10-26 15:55:43 +0800

    修正翻译

--- src/components/VNav3.vue
+++ src/components/VNav3.vue
@@ -86,14 +86,6 @@
                 </li>
                 <li class="dropdown-item pl-24 pr-24">
                   <router-link
-                    :to="{name: 'kycRelay'}"
-                    target="_blank"
-                    class="link">{{ $t("activity_kyc_name") }}
-                    <icon name="hot-red"/>
-                  </router-link>
-                </li>
-                <li class="dropdown-item pl-24 pr-24">
-                  <router-link
                     :to="{name: 'relay'}"
                     target="_blank"
                     class="link">{{ $t("header_title_activity_relay") }}</router-link>
