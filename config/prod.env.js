f369c49fdf87b6725c701b956597663d7b6820bb
mylhei <mylhei@gmail.com>
2019-01-02 17:34:44 +0800

    更改跨域domain

--- config/prod.env.js
+++ config/prod.env.js
@@ -3,6 +3,6 @@ module.exports = {
   NODE_ENV: '"production"',
   STATIC: JSON.stringify(process.env.STATIC),
   // BASE_API: '"http://i.ix-test.com:8080"',
-  BASE_API: '"https://i.ix.com/"',
+  BASE_API: '"https://i.ixx.com/"',
   ...require("./theme.env")
 }
