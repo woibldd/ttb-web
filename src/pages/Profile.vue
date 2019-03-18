263f149d9454d8fd473610f3ab1166648793853f
jaychou2001 <413275531@qq.com>
2019-03-18 20:53:05 +0800

    修改文件名成大写

---
+++ src/pages/Profile.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page page-profile">
+    <div style="page-profile-wrap">
+      <profile-left/>
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProfileLeft from './ProfileLeft'
+import responsiveScale from '@/mixins/responsiveScale'
+export default {
+  mixins: [responsiveScale],
+  name: 'Profile',
+  components: { 
+    ProfileLeft
+  }
+}
+</script>
+<style lang="scss" scoped>
+.page-profile {
+  background: #ffffff;
+  margin: 0 auto;
+  width: 1180px;
+  padding-top: 60px;
+  padding-bottom: 100px;
+}
+
+.mobile {
+  .page-profile {
+    width: 100%;
+  }
+}
+
+</style>
