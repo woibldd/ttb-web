dc54aedf31aa2ee5c102c8d49edfe24462c2d93f
Your Name <you@example.com>
2019-04-18 18:20:45 +0800

    +icon

--- src/components/VNav3.vue
+++ src/components/VNav3.vue
@@ -13,7 +13,7 @@
             class="nav_link">{{ $t('trading') }}</router-link>
             <router-link
               :to="{name: 'RushBuy'}"
-              class="nav_link" style="padding-left:10px;">抢购专区(SP) <img src="@/assets/hot.png" alt="" style="position: relative;top: 5px;left: 5px;"></router-link>
+              class="nav_link" style="padding-left:10px;">{{ $t('FrenzySaleZone') }}(SP)<img src="@/assets/hot.png" alt="" style="position: relative;top: 5px;left: 5px;"></router-link>
             <!-- <div class="nav_link arrow-down">
             <a
               :href="'/docs/IXX+WhitePaper'+pdfSubfix+'.pdf'"
