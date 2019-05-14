b3f31217fdd94a34a3d38036be6d1af3d06b3a8d
woibldd <woibldd@163.com>
2019-05-14 14:49:05 +0800

    添加一个socket心跳

--- src/modules/ws.js
+++ src/modules/ws.js
@@ -24,6 +24,7 @@ export default {
       }
       hub.openTime += 1
       hub.$emit(hub.openTime > 1 ? 'reopen' : 'open')
+      heartCheck.start();
     }
     socket.onmessage = function (evt) {
       if (hub._isDestroyed) {
@@ -43,6 +44,28 @@ export default {
           data = data.data
         }
         hub.$emit('message', data)
+      }
+
+      heartCheck.start();
+    }
+    let heartCheck = {
+      timeout: 10000, //10秒一次
+      timeoutObj: null,
+      serverTimeoutObj: null,
+      start: function() {
+        console.log('start');
+        let self = this;
+        this.timeoutObj && clearTimeout(this.timeoutObj);
+        this.serverTimeoutObj && clearTimeout(this.serverTimeoutObj);
+        this.timeoutObj = setTimeout(function(){
+          //这里发送一个心跳，后端收到后，返回一个心跳消息，
+          //console.log('这里发送一个心跳')
+          socket.send('123456');
+          self.serverTimeoutObj = setTimeout(function() {
+            console.log(socket);
+            socket.close()
+          }, self.timeout)
+        }, self.timeout)
       }
     }
     return hub
