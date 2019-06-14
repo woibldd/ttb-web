0df56327a81c89801e1f7e682d579c41238d6424
woibldd <woibldd@163.com>
2019-06-15 01:54:51 +0800

    合并

--- jenkins.sh
+++ jenkins.sh
@@ -1,16 +1,2 @@
-#!/bin/sh
-
-echo "\nixx.com building...\n"
- 
-
-npm run build
-
-sleep 2
-
-eval `ssh-agent -s`
-ssh-add ~/.ssh/id_rsa
-
-
-rsync -e "ssh -p 10022" -rcvPz dist/* root@47.88.159.32:~/ixx
-
-ssh -p 10022 root@47.88.159.32 "rsync -r ~/ixx/ ixx-www-1:~/ixx"
+npm install --save v-depth-chart      
+npm install --save vue-clipboard2         
