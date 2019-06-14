2314e01bf06746280d08145f831665f6519d8062
woibldd <woibldd@163.com>
2019-06-15 01:57:23 +0800

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,2 +1,16 @@
-cnpm install --save v-depth-chart      
-cnpm install --save vue-clipboard2         
+#!/bin/sh
+
+echo "\nixx.com building...\n"
+ 
+
+npm run build
+
+sleep 2
+
+eval `ssh-agent -s`
+ssh-add ~/.ssh/id_rsa
+
+
+rsync -e "ssh -p 10022" -rcvPz dist/* root@47.88.159.32:~/ixx
+
+ssh -p 10022 root@47.88.159.32 "rsync -r ~/ixx/ ixx-www-1:~/ixx"
