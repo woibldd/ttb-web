24bf47d154cfe1b5fd8e7e71b0bfa81bfb30b5ad
woibldd <woibldd@163.com>
2019-07-05 15:25:57 +0800

    Update config.js

--- src/libs/config.js
+++ src/libs/config.js
@@ -1,17 +1,22 @@
 let wsUrl
 let quoteUrl
 
-if (process.env.NODE_ENV === 'development') {
-  // wsUrl = 'ws://ws.ix-test.com:9090/v1/'
-  // quoteUrl = 'http://q.ix-test.com:8080/v1/'
+
+if(process.env.NODE_ENV != 'development'){
+  let val = location.host.split('.')
+  let host = val.slice(val.length - 2, val.length).join('.')
+  ws = `wss://ws.${host}/v1/` 
+  quote = `https://q.${host}/v1/` 
+}
+if (process.env.NODE_ENV === 'development') { 
   wsUrl = 'wss://ws.ixex.pro/v1/'
   quoteUrl = 'https://q.ixex.pro/v1/'
-} else if (process.env.NODE_ENV === 'beta') {
-  wsUrl = 'wss://ws.ixex.io/v1/'
-  quoteUrl = 'https://q.ixex.io/v1/'
-} else {
-  wsUrl = 'wss://ws.ixex.io/v1/'
-  quoteUrl = 'https://q.ixex.io/v1/'
+} else if (process.env.NODE_ENV === 'beta') { 
+  wsUrl = ws
+  quoteUrl = quote
+} else { 
+  wsUrl = ws
+  quoteUrl = quote
 }
 
 const { exchangeName } = process.env.THEME_ENV
