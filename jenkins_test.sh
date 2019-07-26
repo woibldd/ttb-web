3f78a496e695f1329e5be87ac8c558bb9e7f3c54
woibldd <woibldd@163.com>
2019-07-26 17:42:53 +0800

    Update jenkins_test.sh

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -1,13 +1,14 @@
 #!/bin/sh 
   
-npm run build
+# npm run build
 
-sleep 2
+# sleep 2
 
-eval `ssh-agent -s`
-ssh-add ~/.ssh/id_rsa
+# eval `ssh-agent -s`
+# ssh-add ~/.ssh/id_rsa
 
-rsync -e "ssh -p 10022" -rcvPz dist/* root@47.88.159.32:~/web_staging_ixx
+# rsync -e "ssh -p 10022" -rcvPz dist/* root@47.88.159.32:~/web_staging_ixx
 
-ssh -p 10022 root@47.88.159.32 'rsync -e "ssh -p 10022" -rcvPz ~/web_staging_ixx/ test:/var/www/ixx'
+# ssh -p 10022 root@47.88.159.32 'rsync -e "ssh -p 10022" -rcvPz ~/web_staging_ixx/ test:/var/www/ixx'
  
+ cnpm i -S vue-clipboard2
