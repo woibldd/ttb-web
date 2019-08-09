d5038e83a7a8c85d9de62cdda59a0ea3000101bd
woibldd <woibldd@163.com>
2019-08-09 21:56:01 +0800

    Update Invite.vue

--- src/pages/Invite.vue
+++ src/pages/Invite.vue
@@ -47,8 +47,8 @@
           <div class="invite-list">
             <div class="th pd-15">
               <div class="td">{{ $t('username') }}</div>
-              <div class="td">{{ $t('invite_kyc_lv') }}</div>
-              <div class="td">{{ $t('time') }}</div>
+              <div class="td center">{{ $t('invite_kyc_lv') }}</div>
+              <div class="td right">{{ $t('time') }}</div>
             </div>
             <div class="tbody pb-20">
               <div
@@ -68,7 +68,7 @@
                 <div class="td center">
                   {{ item.lv > 0 ? 'KYC' + item.lv : $t('invite_kyc2_deniel') }}
                 </div>
-                <div class="td">{{ item.register_time | ts2date }}</div>
+                <div class="td right">{{ item.register_time | ts2date }}</div>
               </div>
             </div>
             <div
