38412f13d7bccdcb5e2460ea645d27de35134107
woibldd <woibldd@163.com>
2019-09-09 17:16:03 +0800

    修复娱乐城页面火狐打开时banner显示一半的bug

--- src/pages/Bid/playBTC.vue
+++ src/pages/Bid/playBTC.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="play-btc">
-    <el-carousel :interval="5000" arrow="always" style="z-index: 0;">
+    <el-carousel :interval="5000" arrow="always" style="z-index: 0; width:100%;">
       <el-carousel-item v-for="item in 4" :key="item">
         <h3>{{ item }}</h3>
       </el-carousel-item>
