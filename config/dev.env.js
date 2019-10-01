1f41968626b362952d973c9554cc028eb09c158c
woibldd@163.com <woibldd@163.com>
2019-10-01 15:06:36 +0800

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,8 +6,8 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   STATIC: '"/"',
   //BASE_API: '"http://i.ix-test.com:8080"',
-  //BASE_API: '"https://i.ixex.io"',
+  BASE_API: '"https://i.ixex.io"',
   // BASE_API: '"https://i.ixx.com"',
-  BASE_API: '"https://i.ixex.pro"',
+//   BASE_API: '"https://i.ixex.pro"',
   ...require("./theme.env")
 })
