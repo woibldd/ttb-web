03aa58f120016e0a95c68eaf75a2817894f95c21
woibldd <woibldd@163.com>
2019-10-09 20:03:59 +0800

    Update jenkins_test.sh

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -1,12 +1,14 @@
 #!/bin/sh 
-npm run build
+# npm run build
 
-sleep 2
+# sleep 2
 
-eval `ssh-agent -s`
-ssh-add ~/.ssh/id_rsa
+# eval `ssh-agent -s`
+# ssh-add ~/.ssh/id_rsa
 
-rsync -e "ssh -p 10022" -rcvPz dist/* root@47.74.255.117:~/web_staging_ixx
+# rsync -e "ssh -p 10022" -rcvPz dist/* root@47.74.255.117:~/web_staging_ixx
 
-ssh -p 10022 root@47.74.255.117 'rsync -e "ssh -p 10022" -rcvPz ~/web_staging_ixx/ test:/var/www/ixx'
+# ssh -p 10022 root@47.74.255.117 'rsync -e "ssh -p 10022" -rcvPz ~/web_staging_ixx/ test:/var/www/ixx'
   
+
+cnpm i @amcharts/amcharts4 -S
