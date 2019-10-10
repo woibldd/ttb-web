28c4d2175ad8d9659c75398fdf9e3b757d13bb40
woibldd <woibldd@163.com>
2019-10-10 10:26:13 +0800

    Update jenkins_test.sh

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -1,14 +1,16 @@
 #!/bin/sh 
-npm run build
 
-sleep 2
+ cnpm i compression-webpack-plugin
+# npm run build
 
-eval `ssh-agent -s`
-ssh-add ~/.ssh/id_rsa
+# sleep 2
 
-rsync -e "ssh -p 10022" -rcvPz dist/* root@47.74.255.117:~/web_staging_ixx
+# eval `ssh-agent -s`
+# ssh-add ~/.ssh/id_rsa
 
-ssh -p 10022 root@47.74.255.117 'rsync -e "ssh -p 10022" -rcvPz ~/web_staging_ixx/ test:/var/www/ixx'
+# rsync -e "ssh -p 10022" -rcvPz dist/* root@47.74.255.117:~/web_staging_ixx
+
+# ssh -p 10022 root@47.74.255.117 'rsync -e "ssh -p 10022" -rcvPz ~/web_staging_ixx/ test:/var/www/ixx'
    
 # cnpm uninstall element-ui
 # cnpm uninstall @amcharts/amcharts4
