40d8916e94a6e937c141a28c1033e110053a8702
woibldd <woibldd@163.com>
2019-10-31 15:21:21 +0800

    Create Receive.vue

---
+++ src/pages/Receive.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="receive-container">
+    <div class="header"></div>
+    <div class="content"></div>
+    <div class="footer"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rates: []
+    }
+  },
+  created() {
+    let res = await service.getRates({currency: 'BTC'})  
+    if (!!res && !res.code) {
+      this.rates = res.data.BTC
+    }
+  }
+}
+</script>
+
+<style lang="scss" scope>
+.receive-container {
+  .header {
+    height: 450px;
+  }
+  .content {
+
+  }
+  .footer {
+    height: 450px;
+  }
+}
+</style>
