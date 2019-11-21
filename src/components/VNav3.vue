06f30a69f571fdb78f07be857f2febc3781636f1
woibldd <woibldd@163.com>
2019-11-21 11:14:14 +0800

    修改关键字

--- src/components/VNav3.vue
+++ src/components/VNav3.vue
@@ -362,9 +362,9 @@
         </div>
         <!-- 帮助中心中心 -->
         <div class="nav_item help-center" v-popover:popover7>
-          <span class="help mr-15 ml-15" @click="openDefault('help')">
+          <a rel="nofollow" class="help mr-15 ml-15" @click="openDefault('help')">
             <icon name="header-help" />
-          </span>
+          </a>
            <el-popover
               ref="popover7"
               popper-class="nav-list"
@@ -374,13 +374,13 @@
               <div class="pop-dropdown"> 
             <ul class="dropdown-list pt-10 pb-10">
               <li class="dropdown-item pl-24 pr-24">
-                <a class="link" target="_blank" :href="announcementLink">{{ $t('footer_notice') }}</a>
+                <a class="link" target="_blank"  rel="nofollow"  :href="announcementLink">{{ $t('footer_notice') }}</a>
               </li>
               <li class="dropdown-item pl-24 pr-24">
-                <a class="link" target="_blank" :href="helpLink">{{ $t('user_guide') }}</a>
+                <a class="link" target="_blank"  rel="nofollow"  :href="helpLink">{{ $t('user_guide') }}</a>
               </li>
               <li class="dropdown-item pl-24 pr-24">
-                <a class="link" target="_blank" :href="requestLink">{{ $t('footer_request') }}</a>
+                <a class="link" target="_blank"  rel="nofollow"  :href="requestLink">{{ $t('footer_request') }}</a>
               </li>
             </ul>
           </div>
