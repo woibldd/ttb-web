197b4eff78b2e4e4b8eca2b50903aa15f316fd40
woibldd <woibldd@163.com>
2019-12-09 14:20:00 +0800

    Update default.js

--- src/theme/js/default.js
+++ src/theme/js/default.js
@@ -6,29 +6,29 @@ module.exports = {
   locales: ['en', 'zh-CN', 'zh-HK', 'ko'],
   themeName: 'default',
   companyName: 'IXX.COM',
-  exchangeName: 'IXX交易所 |  比特币交易所,合约交易所,合约交易所排名,比特币走势,比特币实时行情,永续合约,交割合约,比特币合约,BTC合约,ixx',
+  exchangeName: 'IXX |  IXX | Rhino Exchange-BTC Contract Exchange Ranking-Bitcoin Price Real-Time Quote Trend Today-Perpetual Delivery Contract',
   exchangeNameConfig: {
-    'zh-CN': 'IXX交易所 |  比特币交易所,合约交易所,合约交易所排名,比特币走势,比特币实时行情,永续合约,交割合约,比特币合约,BTC合约,ixx',
-    'zh-HK': 'IXX交易所|比特幣交易所，合約交易所，合約交易所排名，比特幣走勢，比特幣即時行情，永續合約，交割合約，比特幣合約，BTC合約，ixx',
+    'zh-CN': 'IXX交易所 | 比特币交易所,合约交易所,合约交易所排名,比特币走势,比特币实时行情,永续合约,交割合约,比特币合约,BTC合约,ixx',
+    'zh-HK': 'IXX交易所 | 比特幣交易所，合約交易所，合約交易所排名，比特幣走勢，比特幣即時行情，永續合約，交割合約，比特幣合約，BTC合約，ixx',
     en: 'IXX |  IXX | Rhino Exchange-BTC Contract Exchange Ranking-Bitcoin Price Real-Time Quote Trend Today-Perpetual Delivery Contract',
-    ko: 'IXX | 수자 재산 거래 공동체'
+    ko: 'IXX | IXX 수자 재산 거래 공동체'
   },
-  keywords: '比特币交易所,合约交易所,合约交易所排名,比特币走势,比特币实时行情,永续合约,交割合约,比特币合约,BTC合约,犀牛交易所',
+  keywords: 'Bitcoin exchange, contract exchange, contract exchange ranking, bitcoin trend, real-time bitcoin market, perpetual contract, delivery contract, bitcoin contract, BTC contract, rhino exchange',
   keywordsConfig: {
     'zh-CN': '比特币交易所,合约交易所,合约交易所排名,比特币走势,比特币实时行情,永续合约,交割合约,比特币合约,BTC合约,犀牛交易所',
     'zh-HK': '數位資產交易所，犀牛交易所，數位資產交易共同體',
     en: ' Bitcoin exchange, contract exchange, contract exchange ranking, bitcoin trend, real-time bitcoin market, perpetual contract, delivery contract, bitcoin contract, BTC contract, rhino exchange',
     ko: 'digital currency exchange, ixx exchange, digital asset trading community'
   },
-  description: 'IXX交易所是全球首家数字资产交易共同体，总部在新加坡，其致力于实现交易所生态的区块链理想国，打造人人的交易所，我为人人，人人为我，所有参与交易所的贡献者均可共享交易所的发展成果。',
+  description: 'IXX | Rhino Exchange is the world\'s first digital asset trading community，headquartered in Singapore. It is committed to achieving the ideal state of the blockchain ecosystem of the exchange and building an exchange for everyone. I am for all and everyone is for me. , All contributors participating in the exchange can share the development results of the exchange.',
   descriptionConfig: {
-    'zh-CN': 'IXX交易所是全球首家数字资产交易共同体，总部在新加坡，其致力于实现交易所生态的区块链理想国，打造人人的交易所，我为人人，人人为我，所有参与交易所的贡献者均可共享交易所的发展成果。',
+    'zh-CN': 'IXX是全球首家数字资产交易共同体，总部在新加坡，其致力于实现交易所生态的区块链理想国，打造人人的交易所，我为人人，人人为我，所有参与交易所的贡献者均可共享交易所的发展成果。',
     'zh-HK': 'IXX交易所是全球首家數位資產交易共同體，總部在新加坡，其致力於實現交易所生態的區塊鏈理想國，打造人人的交易所，我為人人，人人為我，所有參與交易所的貢獻者均可共亯交易所的發展成果。',
     en: "IXX | Rhino Exchange is the world's first digital asset trading community，headquartered in Singapore. It is committed to achieving the ideal state of the blockchain ecosystem of the exchange and building an exchange for everyone. I am for all and everyone is for me. , All contributors participating in the exchange can share the development results of the exchange.",
     ko: ' The world\'s first digital asset trading community; using advanced memory algorithms, distributed information transmission framework, can match large-scale transactions in real time; 100% cold wallet storage mechanism transactions are more secure. Provide digital asset trading services such as Bitcoin, Ethereum, Litecoin, BCH, EOS, etc.'
   },
   shortcut: '',
-  supportMail: 'support@ixx.com',
+  supportMail: 'support@ixx.com', 
   hrMail: 'hr@ixx.com',
   apiDoc: {
     en: 'https://github.com/sonsea/ixx-API-Docs',
