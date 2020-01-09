23b1d19963dd4a7df3f17ab62489edeedb1b3024
woibldd <woibldd@163.com>
2020-01-09 10:16:54 +0800

    修改链接配置

--- src/libs/config.js
+++ src/libs/config.js
@@ -14,13 +14,13 @@ if (process.env.NODE_ENV != 'development') {
     quote = `https://q.${host}/v1/`
     wss = `wss://wss.${host}/v1`
   } else {
-    // ws = 'wss://ws.ixex.io/v1/'
-    // quote = 'https://q.ixex.io/v1/'
-    // wssUrl = 'wss://wss.ixex.io/v1'
+    ws = 'wss://ws.ixex.io/v1/'
+    quote = 'https://q.ixex.io/v1/'
+    wssUrl = 'wss://wss.ixex.io/v1'
     
-    wsUrl = 'wss://ws.ixex.pro/v1/'
-    quoteUrl = 'https://q.ixex.pro/v1/'
-    wssUrl = 'wss://wss.ixex.pro/v1'
+    // wsUrl = 'wss://ws.ixex.pro/v1/'
+    // quoteUrl = 'https://q.ixex.pro/v1/'
+    // wssUrl = 'wss://wss.ixex.pro/v1'
   }
 }
 if (process.env.NODE_ENV === 'development') {
