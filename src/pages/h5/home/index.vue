522c90a0e681ff5156b6bcd4e5fd0fa9100148d4
woibldd <woibldd@163.com>
2020-07-07 15:09:11 +0800

    Update index.vue

--- src/pages/h5/home/index.vue
+++ src/pages/h5/home/index.vue
@@ -495,11 +495,15 @@ export default {
       if (type==='android') {
         let url = 'https://upgrade-app.oss-cn-hangzhou.aliyuncs.com/two/ixx.apk' 
         window.location.href = url
-      } 
+      }
+      else if (type==='ios-test') {
+        let url = 'https://testflight.apple.com/join/yCUsQxlN' 
+        window.location.href = url 
+      }
       else if (this.isIos) {
         let url = ''
         if(type==='ios-test') {
-        let url = 'https://testflight.apple.com/join/yCUsQxlN' 
+          url = 'https://testflight.apple.com/join/yCUsQxlN' 
         } else { 
           url = 'itms-services://?action=download-manifest&url=https://upgrade-app.oss-cn-hangzhou.aliyuncs.com/two/install-manifest.plist'
         } 
