5b995a97a0aa3f2bba6bb480c4010d46e504b24d
woibldd <woibldd@163.com>
2020-08-14 11:59:49 +0800

    调整期权账户样式，显示账户余额

--- src/components/Fund/My/share_history.vue
+++ src/components/Fund/My/share_history.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="property-manage-warp"
        flex="dir:top">
-    <div class="top"
-         flex="main:justify cross:center">
-      <div class="title"
-           style="font-size: 18px;text-align: left;">{{$t('shareOption.share_account')}}</div>
-      <customForm ref="customForm"
+    <div class="top mb-30" flex="main:justify cross:center">
+      <div class="title" style="font-size: 18px;text-align: left;">
+        {{$t('shareOption.share_account')}}
+      </div>
+      <div class="right">
+        <label @click="currentType='account'" :class="{active: currentType==='account'}" class="fund-history mr-20">账户余额</label>
+        <label @click="currentType='history'" :class="{active: currentType==='history'}" class="fund-history mr-20">交易记录</label>
+      </div> 
+      <!-- <customForm ref="customForm"
                   size="mini"
                   flex="main:justify"
                   class="login-custom-form"
@@ -13,15 +17,79 @@
                   :submit-btn="false"
                   label-width="10"
                   label-position="left">
-      </customForm>
+      </customForm> -->
+
       <!-- <el-divider style="maring:12px 0" /> -->
+    </div> 
+    <div class="filter mb-10" v-if="currentType==='history'"> 
+      <div class="label mr-20">
+        <label for="">{{$t('order_th_type')}}</label> 
+      </div>
+      <div class="mr-30">
+        <el-select  
+          size="small"
+          v-model="search.currency" 
+          :placeholder="schema[0].placeholder"
+          @change="handlePageChange(1)" >
+          <el-option
+            v-for="item in this.schema[0].options"
+            :key="item" 
+            :label="item"
+            :value="item">
+          </el-option>
+        </el-select>  
+      </div>
+      <div class="label mr-20">
+        <label for="">{{$t('date')}}</label> 
+      </div>
+      <div> 
+        <el-date-picker
+          size="small"
+          v-model="search.dataRange" 
+          @change="handlePageChange(1)" 
+          type="daterange"
+          value-format="timestamp"
+          range-separator="-"
+          :start-placeholder="$t('fund.contract.start')"
+          :end-placeholder="$t('fund.contract.end')">
+        </el-date-picker> 
+      </div> 
     </div>
-    <custom-table v-loading="loading"
-                  stripe
-                  :table-list="tableList"
-                  :table-columns="mapShareColumns"
-                  @change="handlePageChange">
-    </custom-table>
+    <div class="content">
+      <div class="history" v-if="currentType==='history'">
+        <el-table v-loading="loading" :data="tableList" style="width: 100%;">
+          <el-table-column 
+            v-for="(item, key) in mapShareColumns" 
+            :label="item.hearderLabel" 
+            :width="item.hearderWidth(item.prop)"
+            :prop="item.prop"
+            :key="key"> 
+            <template slot-scope="scope">
+              <div>
+                {{ item.handleValue(scope.row[item.prop], item.prop) }} 
+              </div>
+            </template>
+          </el-table-column> 
+        </el-table>
+        <el-pagination 
+          class="mt-20"
+          background
+          layout="prev, pager, next"
+          @current-change="handlePageChange"
+          :current-page.sync="pageConfig.page"
+          :page-size="pageConfig.size"
+          :total="pageConfig.total">
+        </el-pagination> 
+      </div>
+      <div class="account" v-if="currentType==='account'">
+        <el-table  v-loading="loading" :data="accountList" style="width: 100%;">
+           <el-table-column :label="$t('currency')" prop="currency"></el-table-column>
+           <el-table-column :label="$t('avlb')" prop="available"></el-table-column>
+           <el-table-column :label="$t('locked')" prop="ordering"></el-table-column> 
+        </el-table>
+      </div>
+    </div>
+    
   </div>
 </template>
 <script>
@@ -60,15 +128,26 @@ export default {
           }        
         },
         // { fieldType: 'date-picker',size:'mini',type:'daterange', prefixIcon: 'el-icon-search', valueFormat: 'timestamp', placeholder: '', vModel: 'plan_time', default: '' }
-      ]
+      ],
+      pageConfig: {
+        page: 1,
+        size: 10,
+        total: 0
+      },
+      search: {
+        currency: '',  
+        dataRange: ''
+      },
+      currentType: 'account'
     }
   },
   computed: {
     mapShareColumns () {
       return Object.keys(this.chineseLangData.mapShareColumns).map(key => ({
-        hearderLabel: this.$tR(`mapShareColumns.${key}`),
+        hearderLabel: this.$t(`shareOption.mapShareColumns.${key}`),
         prop: key,
-        hearderWidth: key => ['period', 'trade_type', 'amount', 'income', 'profile', 'rate'].includes(key) && '50px',
+        hearderWidth: key => ['create_time', 'sett_time',].includes(key) && 95, // key => ['period', 'trade_type', 'amount', 'income', 'profile', 'rate'].includes(key) && '70px',
+
         handleValue: (value, key) => {
           switch (key) {
             case 'period':
@@ -104,23 +183,30 @@ export default {
 
   },
   created () {
+    this.handlePageChange(this.pageConfig.page)
     this.$store.dispatch('getShareAccountList').then(res => {
       this.schema[0].options = res.map(item => item.currency)
       this.schema[0].placeholder = this.$tR('account')
+      this.accountList = res
       // this.schema[1].placeholder = this.$tR('time')
     })
   },
   methods: {
-    handlePageChange (pageConfig) {
-      this.temPageConfig = pageConfig || this.temPageConfig
-      if (!pageConfig) this.temPageConfig.init()
-      const { pageSize, currentPage } = this.temPageConfig
-      this.loading = true
-      const currency = this.schema[0].currency 
+    handlePageChange (page) { 
+      this.loading = true 
+      const params = {
+        currency: this.search.currency,
+        page, 
+        size: this.pageConfig.size 
+      }
+      if (this.search.dataRange) {
+        params.begin_time = this.search.dataRange[0]
+        params.end_time = this.search.dataRange[1]
+      }
 
-      getHistory({ user_id: this.id, page: currentPage, size: pageSize, currency }).then(res => {
-        this.tableList = res.data.data
-        this.temPageConfig.total = res.data.total
+      getHistory(params).then(res => {
+        this.tableList = res.data.data 
+        this.pageConfig.total = res.data.total 
         this.loading = false
       })
     }
@@ -131,6 +217,10 @@ export default {
 .property-manage-warp {
   height: 100%;
   text-align: center;
+  .filter {
+    display: flex;
+    align-items: center;
+  }
   & > .center {
     .numerical {
       // line-height: 100px;
