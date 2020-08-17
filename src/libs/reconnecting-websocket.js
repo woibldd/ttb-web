4bd99b03ecbf3dd7dce762060a9d512351ccc34a
woibldd <woibldd@163.com>
2020-08-17 14:26:34 +0800

    修改socket超时设置

--- src/libs/reconnecting-websocket.js
+++ src/libs/reconnecting-websocket.js
@@ -126,7 +126,7 @@
             reconnectDecay: 1.5,
 
             /** The maximum time in milliseconds to wait for a connection to succeed before closing and retrying. */
-            timeoutInterval: 2000,
+            timeoutInterval: 8000,
 
             /** The maximum number of reconnection attempts to make. Unlimited if null. */
             maxReconnectAttempts: null,
