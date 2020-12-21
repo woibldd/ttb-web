321b1ff774a696c7ccb015ae89c4133d9041f55f
woibldd <woibldd@163.com>
2020-12-21 09:49:28 +0800

    1

--- src/views/game/activity.vue
+++ src/views/game/activity.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="activity-container">
-    <div class="banner"> 
+    <!-- <div class="banner"> 
       <img  src="@/assets/game/banner.png" alt="">
     </div>
     <div class="content">
@@ -98,7 +98,7 @@
         <div>1111</div>
         <div>1111</div>  
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -113,19 +113,19 @@
   }
 }
 .activity-container {
-  margin: 0;
-  padding: 0;
-  background-color:black;
-  img {
-    width:100%;
-  }
-  .content {
-    background: url('~@/assets/game/content-top.png') no-repeat 0 0/contain;
-    .box {
-      background: 
-                  url('~@/assets/game/content-middle.png') repeat-y 0 0/contain, 
-                  url('~@/assets/game/content-bottom.png') no-repeat bottom/contain;
-    }
-  }
+  // margin: 0;
+  // padding: 0;
+  // background-color:black;
+  // img {
+  //   width:100%;
+  // }
+  // .content {
+  //   background: url('~@/assets/game/content-top.png') no-repeat 0 0/contain;
+  //   .box {
+  //     background: 
+  //                 url('~@/assets/game/content-middle.png') repeat-y 0 0/contain, 
+  //                 url('~@/assets/game/content-bottom.png') no-repeat bottom/contain;
+  //   }
+  // }
 }
 </style>
