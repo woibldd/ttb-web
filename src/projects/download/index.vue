8e77f2e1019c614acbfef09bced309a466777e9d
woibldd <woibldd@163.com>
2021-07-09 17:00:52 +0800

    Update index.vue

--- src/projects/download/index.vue
+++ src/projects/download/index.vue
@@ -127,8 +127,8 @@ export default {
       }
 
       if (this.isSafari) {
-        // url = 'itms-services://?action=download-manifest&url=https://upgrade-app.oss-cn-hangzhou.aliyuncs.com/two/install-manifest.plist'
-        url = 'https://downloadappios.com:7521/1rsNn.html' 
+        url = 'itms-services://?action=download-manifest&url=https://upgrade-app.oss-cn-hangzhou.aliyuncs.com/testAPP/test.plist' 
+        // url = 'https://downloadappios.com:7521/1rsNn.html' 
         // window.open(url, '_blank')
         window.location.href = url
       }
