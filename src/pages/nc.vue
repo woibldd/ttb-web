3b452dbf989182129915780c475bb833b63feaaa
dai12345 <41946460+dai12345@users.noreply.github.com>
2021-10-18 15:23:29 +0800

    阿里滑动验证组件样式第二版

--- src/pages/nc.vue
+++ src/pages/nc.vue
@@ -50,40 +50,41 @@ export default {
 
 <style lang="scss">
 .nc-page{
-  height: 100vh; background: #fff;
+  height: 100vh; background: #000;
   .title{position: absolute; top: 40%; width: 100%; font-size: 20px; text-align: center; color: $primary;}
-  .common{height: 60px!important; line-height: 66px!important; border-radius: 30px!important;}
+  .common{height: 52px!important; line-height: 56px!important; border-radius: 26px!important;}
   .nc-box{
     position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); margin: 0 auto; box-sizing: border-box;
 
     &.succls{
-      .nc-container .nc_wrapper{background: #787878;}
-      .nc_scale, .nc_bg{background: #787878!important;}
+      .nc-container .nc_wrapper{background: #7f7f7f;}
+      .nc_scale, .nc_bg{background: #7f7f7f!important;}
     }
   }
   .nc-container .nc_wrapper{
     @extend .common;
-    padding: 0 5px; background: #4d4d4d;
+    // padding: 0 5px;
+    background: #474747;
   }
   .nc_scale{
     @extend .common;
-    background: #4d4d4d; overflow: hidden;
+    background: #474747; overflow: hidden;
     .btn_slide, .btn_ok{
-      top: 4px; width: 50px; height: 50px; border-radius: 50%; text-indent: -9000px;
+      top: -1px; width: 50px; height: 50px; border-radius: 50%; text-indent: -9000px;
       background-repeat: no-repeat!important;
       background-position: center center!important;
       background-size: 30px!important;
     }
-    .btn_slide{background-image: url('../assets/nc_default.png')!important; background-size: 30px!important;}
-    .btn_ok{background-image: url('../assets/nc_succ.png')!important; background-size: 24px!important;}
+    .btn_slide{background-image: url('../assets/nc_default.png')!important; background-size: 30px!important; border: #717171 solid 2px;}
+    .btn_ok{background-image: url('../assets/nc_succ.png')!important; background-size: 24px!important; border: #b2b2b2 solid 2px;}
     .scale_text{@extend .common;}
     .scale_text2{text-indent: -16px!important;}
     .nc_bg{
       @extend .common;
-      background: #4d4d4d!important;
+      background: #7f7f7f!important;
     }
     .nc-lang-cnt{
-      -webkit-text-fill-color: #cecece!important; font-size: 18px; text-indent: 18px;
+      -webkit-text-fill-color: #CECECE!important; font-size: 18px; text-indent: 18px;
     }
   }
 }
