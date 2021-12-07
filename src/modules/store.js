580ed3d4a2fc45e2ade1969087a0d26ac09ccbe3
woibldd <woibldd@163.com>
2021-12-07 12:05:06 +0800

    Update store.js

--- src/modules/store.js
+++ src/modules/store.js
@@ -24,7 +24,7 @@ export const state = {
     android: 'https://upgrade-app.oss-cn-hangzhou.aliyuncs.com/two/ixx.apk',
     // ios: 'https://ssfdld.com?onpYQ',
     // ios: 'itms-services://?action=download-manifest&url=https://upgrade-app.oss-cn-hangzhou.aliyuncs.com/two/install-manifest.plist',
-    ios: 'https://mbdp.qasdds.com/onpYQ.html'
+    ios: 'https://bnyo.nywdo.com/onpYQ.html'
   },
   assets: {
     ready: false,
