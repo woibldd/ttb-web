c739e4d840106fe7334f285f7a7bbcce41935d75
woibldd <42069928+woibldd@users.noreply.github.com>
2022-08-28 11:56:54 +0800

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,31 +1,7 @@
-# #!/bin/sh
-# echo "\nttb.plus building...\n"
- 
-
-# cnpm i vue-verification-code-input -S
-
-# sleep 2
-
-# npm run build
-
-# sleep 2
-
-# eval `ssh-agent -s`
-# ssh-add ~/.ssh/id_rsa
-
-
-# rsync -e "ssh -p 10022" -rcvPz dist/* root@47.74.255.117:~/ttb
-
-# ssh -p 10022 root@47.74.255.117 "rsync -r ~/ttb/ ixx-www-1:~/ttb"
-
-#!/bin/sh
-npm install chalk   
-sleep 2
- 
 echo "\nttb.plus building...\n"  
 npm run build
 sleep 2
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa 
 rsync -e "ssh -p 10022" -rcvPz dist/* root@47.74.255.117:~/site_11
-ssh -p 10022 root@47.74.255.117 'rsync -e "ssh -p 10022" -rcvPz ~/site_11/ 47.244.141.131:~/site_11/'
+ssh -p 10022 root@47.74.255.117 'rsync -e "ssh -p 22" -rcvPz ~/site_11/ 10.12.54.154:~/site_11/'
