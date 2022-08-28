afd39355516c9e2f082769fb5e7c037c451df75c
woibldd <42069928+woibldd@users.noreply.github.com>
2022-08-28 11:59:19 +0800

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,4 +4,4 @@ sleep 2
 eval `ssh-agent -s`
 ssh-add ~/.ssh/id_rsa 
 rsync -e "ssh -p 10022" -rcvPz dist/* root@47.74.255.117:~/site_11
-ssh -p 10022 root@47.74.255.117 'rsync -e "ssh -p 22" -rcvPz ~/site_11/ 10.12.54.154:~/site_11/'
+ssh -p 10022 root@47.74.255.117 'rsync -e "ssh -p 22" -rcvPz ~/site_11/ 10.12.54.154:~/site_11/' 
